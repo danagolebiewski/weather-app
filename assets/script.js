76e543ded51177e0cc338dcf091660e237b6b006
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-08-01 10:40:11 -0600

    added 5 day forecast cards, and dates

--- assets/script.js
+++ assets/script.js
@@ -6,19 +6,6 @@ var cityInput = $('input[name="city"]');
 
 // first try to make an API call with code (you're going to give the values that are needed)
 
-fetch(
-  `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`
-);
-
-// // Make function for fetch current weather
-// fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Boulder&limit=1&appid=${API_KEY}`)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     // console.log(data);
-//   });
-
 // fetch for current weather
 function apiCall(city) {
   console.log("hello");
@@ -29,6 +16,7 @@ function apiCall(city) {
       return response.json();
     })
     .then(function (data) {
+      $("#date").text(new Date(data.dt*1000).toLocaleString('en-US', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'}));
       $("#displayboxtitle").text(city);
       $("#temp").text(`Temp: ${data.main.temp}`);
       $("#wind").text(`Wind: ${data.wind.speed}`);
@@ -36,19 +24,33 @@ function apiCall(city) {
 
       // if we need data from a previous API call we have to WAIT for that data to be returned to us
     });
-
 }
 
 function fiveDayApiCall(city) {
-  console.log("hello");
   fetch(
-    `https://api.openweathermap.org/forecast/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data.list);
+      for (let index = 6; index < data.list.length; index += 8) {
+        const element = data.list[index];
+        console.log(element);
+
+        let card = document.createElement("div");
+        card.classList.add("card", "col", "m-1");
+        card.innerHTML = `
+          <div class="card-body">
+          <p class="card-title">${new Date(element.dt*1000).toLocaleString('en-US', {weekday: 'long'})}</p>
+          <p class="card-text">Temp ${element.main.temp}</p>
+          <p class="card-text">Wind ${element.wind.speed}</p>
+          <p class="card-text">Humidity ${element.main.humidity}</p>
+          </div
+        `;
+        document.getElementById('5-day').append(card)
+      }
       // $("#displayboxtitle").text(city);
       // $("#temp").text(`Temp: ${data.main.temp}`);
       // $("#wind").text(`Wind: ${data.wind.speed}`);
@@ -56,7 +58,6 @@ function fiveDayApiCall(city) {
 
       // if we need data from a previous API call we have to WAIT for that data to be returned to us
     });
-
 }
 
 function handleFormSubmit(event) {
@@ -64,14 +65,12 @@ function handleFormSubmit(event) {
   var cityName = cityInput.val();
   console.log(cityName);
   apiCall(cityName);
-  fiveDayApiCall();
+  fiveDayApiCall(cityName);
 }
 
-formEl.on('submit', handleFormSubmit);
+formEl.on("submit", handleFormSubmit);
 
-
-
-// Call 5 day forecast, add info to cards, 
-// Add search history with active buttons 
+// Call 5 day forecast, add info to cards,
+// Add search history with active buttons
 // Find UVI with color changing button thingy
-// Add weather indicator symbol 
+// Add weather indicator symbol
